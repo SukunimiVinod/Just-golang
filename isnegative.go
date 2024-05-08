431f29c2ceea0cfdd6a4be4fa21912bb2ba43f2d
vvinod <Vinodvinod19032022@gmail.com>
2024-05-08 16:53:10 +0300

    isnegavtuive

--- isnegative.go
+++ isnegative.go
@@ -1,14 +1,14 @@
 package piscine
-//somecomment to check
+
+// somecomment to check
 import "github.com/01-edu/z01"
 
+func IsNegative(nb int) {
+	if nb < 0 {
+		z01.PrintRune('T')
+	} else {
+		z01.PrintRune('F')
+	}
 
-func IsNegative (nb int) {
-		if nb < 0 {
-			z01.PrintRune('T')
-		} else {
-			z01.PrintRune('F')
-		}
-
-		z01.PrintRune('\n')
+	z01.PrintRune('\n')
 }
